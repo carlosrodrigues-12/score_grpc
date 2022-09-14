1011aade26ddc66332918c12f733e740e614d0f1
carlosrodrigues-12 <carlos_rodrigues@discente.ufg.br>
2022-09-14 19:50:01 -0300

    Servicer

--- python/score.py
+++ python/score.py
@@ -57,7 +57,7 @@ class Message(score_pb2_grpc.MessageServicer):
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    score_pb2_grpc.add_MessageServicer_to_server(Message(), server)
+    score_pb2_grpc.add_OperacoesServicer_to_server(Message(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
