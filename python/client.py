01ba4222d41bf00a67c8eedc79a213f027b3f280
carlosrodrigues-12 <carlos_rodrigues@discente.ufg.br>
2022-09-14 20:19:09 -0300

    Identify

--- python/client.py
+++ python/client.py
@@ -4,18 +4,20 @@ import logging
 import grpc
 import score_pb2
 import score_pb2_grpc
-
+import sys
 import const
+global myidentify
 
 def run():
     with grpc.insecure_channel(const.CHAT_SERVER_HOST) as channel:
         stub = score_pb2_grpc.OperacoesStub(channel)
 
         # Score
-        response = stub.ConsultCurrentScore(score_pb2.EmptyMessage())
+        response = stub.ConsultCurrentScore(score_pb2.ConsultPlayer(player=myidentify))
         # print ('Employee\'s data: ' + str(response))
         print(response)
 
 if __name__ == '__main__':
     logging.basicConfig()
+    myip = str(sys.argv[1])
     run()
