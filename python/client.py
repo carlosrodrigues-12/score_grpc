10ba92e14de2562b8dfb789c025c011904293100
carlosrodrigues-12 <carlos_rodrigues@discente.ufg.br>
2022-09-14 22:10:14 -0300

    INput

--- python/client.py
+++ python/client.py
@@ -1,11 +1,19 @@
 from __future__ import print_function
 import logging
+from urllib import response
 
 import grpc
 import score_pb2
 import score_pb2_grpc
 import sys
 import const
+
+def consulta():
+    with grpc.insecure_channel(const.CHAT_SERVER_HOST) as channel:
+        stub = score_pb2_grpc.OperacoesStub(channel)
+
+    response = stub.ConsultCurrentScore(score_pb2.EmptyMessage())
+    print(f'SCORE ATUAL\n{response}') 
 
 def run(myidentify,point):
     with grpc.insecure_channel(const.CHAT_SERVER_HOST) as channel:
@@ -24,7 +32,11 @@ def run(myidentify,point):
 if __name__ == '__main__':
     logging.basicConfig()
     myidentify = str(sys.argv[1])
+    print(f'PLAYER: {myidentify}')
     while True:
-        print(f'PLAYER: {myidentify}')
-        point = int(input("ENTER POINT: "))
-        run(myidentify,point)
+        op = input("1 CONSULTA - 2 UPDATE SCORE: ")
+        if(op == 1):
+            consulta()
+        else:
+            point = int(input("ENTER POINT: "))
+            run(myidentify,point)
