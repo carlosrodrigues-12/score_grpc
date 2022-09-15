5e4006b4292965ef52c29956175a7f2c2e2663d9
carlosrodrigues-12 <carlos_rodrigues@discente.ufg.br>
2022-09-14 22:21:23 -0300

    Input

--- python/client.py
+++ python/client.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     while True:
         op = int(input("1 CONSULTA - 2 UPDATE SCORE: "))
         if(op == 1):
-            run(myidentify,point=0,op)
+            run(myidentify,point,op)
         else:
             point = int(input("ENTER POINT: "))
             run(myidentify,point,op)
