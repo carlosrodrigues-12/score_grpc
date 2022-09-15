742b9c4855bb43289abb9d7f72211c292f7e24db
carlosrodrigues-12 <carlos_rodrigues@discente.ufg.br>
2022-09-14 22:20:03 -0300

    Input

--- python/client.py
+++ python/client.py
@@ -8,26 +8,22 @@ import score_pb2_grpc
 import sys
 import const
 
-def consulta():
+def run(myidentify,point,op):
     with grpc.insecure_channel(const.CHAT_SERVER_HOST) as channel:
         stub = score_pb2_grpc.OperacoesStub(channel)
 
-    response = stub.ConsultCurrentScore(score_pb2.EmptyMessage())
-    print(f'SCORE ATUAL\n{response}') 
+        if (op == 1):
+            response = stub.ConsultCurrentScore(score_pb2.EmptyMessage())
+            print(f'SCORE ATUAL\n{response}')
+        else:
+            response = stub.ConsultCurrentScore(score_pb2.EmptyMessage())
+            print(f'SCORE ATUAL\n{response}')
 
-def run(myidentify,point):
-    with grpc.insecure_channel(const.CHAT_SERVER_HOST) as channel:
-        stub = score_pb2_grpc.OperacoesStub(channel)
+            calc = stub.CalcNewScore(score_pb2.Player(player=myidentify,point=point))
+            print(f'CALC SCORE {myidentify} = {calc.point}')
 
-        # Score
-        response = stub.ConsultCurrentScore(score_pb2.EmptyMessage())
-        print(f'SCORE ATUAL\n{response}')
-
-        calc = stub.CalcNewScore(score_pb2.Player(player=myidentify,point=point))
-        print(f'CALC SCORE {myidentify} = {calc.point}')
-
-        upd = stub.UpdateScore(score_pb2.Player(player=myidentify,point=calc.point))
-        print(f'NEW SCORE\n{upd}')
+            upd = stub.UpdateScore(score_pb2.Player(player=myidentify,point=calc.point))
+            print(f'NEW SCORE\n{upd}')
 
 if __name__ == '__main__':
     logging.basicConfig()
@@ -36,7 +32,7 @@ if __name__ == '__main__':
     while True:
         op = int(input("1 CONSULTA - 2 UPDATE SCORE: "))
         if(op == 1):
-            consulta()
+            run(myidentify,point=0,op)
         else:
             point = int(input("ENTER POINT: "))
-            run(myidentify,point)
+            run(myidentify,point,op)
