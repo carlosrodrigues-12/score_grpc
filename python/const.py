ba2e9450b422c6d177d1cb4166e9d067be712594
carlosrodrigues-12 <carlos_rodrigues@discente.ufg.br>
2022-09-16 17:35:05 -0300

    Const

--- python/const.py
+++ python/const.py
@@ -1,7 +1,7 @@
-CHAT_SERVER_HOST = "172.17.0.2:50051"
+CHAT_SERVER_HOST = "172.31.16.169:50051"
 
 # IDs de usuários registrados com os respectivos endereços IP
 registry = {
-    "player1":"172.17.0.3:50051",
-    "player2":"172.17.0.4:50051"
+    "player1":"172.31.31.165:50051",
+    "player2":"172.31.24.150:50051"
 }
